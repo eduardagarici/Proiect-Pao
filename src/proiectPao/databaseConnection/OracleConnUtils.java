878ae1891eea64cd0464f296b99272f60bb52071
eduardagarici <34513592+eduardagarici@users.noreply.github.com>
2018-05-16 22:27:30 +0300

    database online

---
+++ src/proiectPao/databaseConnection/OracleConnUtils.java
@@ -0,0 +1,33 @@
+package proiectPao.databaseConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
+public class OracleConnUtils {
+ 
+    // Connect to Oracle.
+    public static Connection getOracleConnection() throws SQLException,
+            ClassNotFoundException {
+        String hostName = "193.226.51.37";
+        String sid = "o11g";
+        String userName = "grupa241";
+        String password = "bazededate";
+        return getOracleConnection(hostName, sid, userName, password);
+    }
+ 
+    public static Connection getOracleConnection(String hostName, String sid,
+            String userName, String password) throws ClassNotFoundException,
+            SQLException {
+        
+        // Declare the class Driver for ORACLE DB
+        // This is necessary with Java 5 (or older)
+        // Java6 (or newer) automatically find the appropriate driver.
+        // If you use Java> 5, then this line is not needed.        
+        String connectionURL = "jdbc:oracle:thin:@" + hostName + ":1521:" + sid;
+ 
+        Connection conn = DriverManager.getConnection(connectionURL, userName,
+                password);
+        return conn;
+    }
+}
